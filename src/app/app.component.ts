e617e07ecdb48e40f7883042ea1b31a734af6ece
Sebastian Hammer <mail@hammer-sebastian.de>
2021-04-21 17:43:29 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Exercise} from './exercise';
+import {ExerciseService} from './exercise.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fitnesstracker-frontend';
+export class AppComponent implements OnInit {
+  public exercises: Exercise[] | undefined;
+
+  constructor(private exerciseService: ExerciseService) {
+  }
+
+  ngOnInit(): void {
+    this.getExercises();
+  }
+
+  public getExercises(): void {
+
+    this.exerciseService.getExercises().subscribe(
+      (response: Exercise[]) => {
+        this.exercises = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
---
+++ src/app/exercise.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Exercise} from './exercise';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExerciseService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getExercises(): Observable<Exercise[]> {
+    return this.http.get<Exercise[]>(`${this.apiServerUrl}/exercise/all`);
+  }
+
+  public addExercises(exercise: Exercise): Observable<Exercise> {
+    return this.http.post<Exercise>(`${this.apiServerUrl}/exercise/add`, exercise);
+  }
+
+  public updateExercises(exercise: Exercise): Observable<Exercise> {
+    return this.http.put<Exercise>(`${this.apiServerUrl}/exercise/update`, exercise);
+  }
+
+  public deleteExercises(exerciseId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/exercise/delete/${exerciseId}`);
+  }
+
+}
